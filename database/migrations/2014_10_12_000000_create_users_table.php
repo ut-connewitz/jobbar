79d21fb1abab1836a83c54edefaa92184b0ef372
Simeon Ackermann <amseon@web.de>
2019-03-20 21:08:40 +0100

    Fix name duplication is role, #1

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('name', 20)->default("authenticated");
+            $table->string('role', 20)->default("authenticated");
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
